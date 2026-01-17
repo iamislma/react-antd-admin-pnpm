779cf6ee8b3de35d8fb1407a33583e65d2779ef4
1900s88keys <rancong_hot@163.com>
2026-01-17 22:20:51 +0800

    feat(ui): 题目7: 请在UI子包中，使用react-flow设计一个动态编辑组件

--- packages/ui/modules/index.ts
+++ packages/ui/modules/index.ts
@@ -2,6 +2,7 @@ import AnimatePanel from './AnimatePanel/index';
 import Translatex from './AnimatePanel/Translatex';
 import BaseCard from './Card';
 import DrawBoard from './DrawBoard';
+import FlowEditor from './FlowEditor';
 import GlobalConfig from './GlobalConfig/ConfigProvider';
 import Loading from './Loading';
 import PointCloudViewer from './PointCloudViewer';
@@ -9,4 +10,5 @@ import BaseSlick from './Slick/BaseSlick';
 import SvgIcon from './SvgIcon';
 import PointCloud from './PointCloud';
 
-export { AnimatePanel, BaseCard, BaseSlick, DrawBoard, GlobalConfig, Loading, PointCloud, PointCloudViewer, SvgIcon, Translatex };
+export { AnimatePanel, BaseCard, BaseSlick, DrawBoard, FlowEditor, GlobalConfig, Loading, PointCloud, PointCloudViewer, SvgIcon, Translatex };
+
